7d6caaddf768d11bedbf65bae8ca635c52ee808f
Antonio Kristicevic <kristicevic.antonio@gmail.com>
2018-10-29 20:06:31 +0100

    Register context and interceptors

---
+++ Eferada/DependencyModules/DataIocModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Eferada.DatabaseContext;
+using Eferada.Interceptors;
+using Eferada.Interceptors.Auditable;
+using Eferada.Repository;
+
+namespace Eferada.DependencyModules
+{
+    public class DataIocModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            RegisterContext(builder);
+            RegisterInterceptors(builder);
+
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+
+        private void RegisterInterceptors(ContainerBuilder builder)
+        {
+            builder.RegisterType<EntityInterceptorsDispatcher>().InstancePerLifetimeScope();
+            builder.RegisterType<AuditableInterceptor>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            //Missing dbset interceptor
+        }
+
+        private void RegisterContext(ContainerBuilder builder)
+        {
+            builder.Register(c =>
+                {
+                    var entityChangeInterceptorsDispatcher = c.Resolve<EntityInterceptorsDispatcher>();
+
+                    return new EferadaDbContext(entityChangeInterceptorsDispatcher);
+                })
+                .AsSelf()
+                .As<IEferadaDbContext>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
