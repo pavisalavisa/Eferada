2653eb48715816b804d296a5b7509502f22c2967
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2018-10-29 13:13:21 +0100

    WIP: Implement repository pattern 3

---
+++ Eferada.Common/Exceptions/DatabaseException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Eferada.Common.Exceptions
+{
+    public enum DatabaseErrorCode
+    {
+        Unknown=0,
+        EntityNotFound=1,
+        DeleteError
+    }
+
+    public class DatabaseException:Exception
+    {
+        public DatabaseErrorCode ErrorCode { get; }
+
+        public DatabaseException(DatabaseErrorCode errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public DatabaseException(DatabaseErrorCode errorCode,string message):base (message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public DatabaseException(DatabaseErrorCode errorCode, string message, Exception innerException) : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
