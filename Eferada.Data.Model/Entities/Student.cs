e1791b3710cedff0e6d900a7738578cc285720b0
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2019-01-17 13:56:59 +0100

    Add student entity and status enum

---
+++ Eferada.Data.Model/Entities/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Eferada.Data.Model.Entities.Identity;
+using Eferada.Data.Model.Enums;
+
+namespace Eferada.Data.Model.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Şurname { get; set; }
+        public int AcademicYear { get; set; }
+        public string StudentCardCode { get; set; }
+        public int CourseId { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDateTime { get; set; }
+        public DateTime EnrollmentYear { get; set; }
+        public StudentStatus StudentStatus { get; set; }
+        public string Address { get; set; }// TODO: include Address type in the future
+
+        public ApplicationUser ApplicationUser { get; set; } // TODO: set 1 to 1 relationship http://www.entityframeworktutorial.net/code-first/configure-one-to-one-relationship-in-code-first.aspx
+    }
+}
---
+++ Eferada.Data.Model/Enums/StudentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Eferada.Data.Model.Enums
+{
+    public enum StudentStatus
+    {
+        Regular = 1,
+        PartTime = 2
+
+    }
+}
