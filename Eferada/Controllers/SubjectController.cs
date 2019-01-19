34934beb3b88cc514415051850b7ab2a140f590b
AntonioKristicevic <antonio.kristicevic@typeqast.com>
2019-01-19 16:34:26 +0100

    Add subject controller and business service

---
+++ Eferada/Controllers/SubjectController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using AutoMapper;
+using Business.Services.Contracts;
+using Eferada.Infrastructure;
+
+namespace Eferada.Controllers
+{
+    [EferadaRoutePrefix(nameof(SubjectController))]
+    public class SubjectController : ApiController
+    {
+        private readonly IMapper _mapper;
+        private readonly ISubjectBusinessService _subjectBusinessService;
+
+        public SubjectController(IMapper mapper, ISubjectBusinessService subjectBusinessService)
+        {
+            _mapper = mapper;
+            _subjectBusinessService = subjectBusinessService;
+        }
+    }
+}
